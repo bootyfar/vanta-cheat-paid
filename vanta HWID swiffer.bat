073d0831865b8751fc3fff3cb86c54942d068063
bootyfar <150213095+bootyfar@users.noreply.github.com>
2023-11-29 16:25:03 -0500

    Add files via upload

---
+++ vanta HWID swiffer.bat
@@ -0,0 +1,179 @@
+@echo off
+title vanta HWID 
+color B
+echo spoofer
+ping n- 2 127.0.0.1>nul
+echo you need licens key
+ping n- 2 127.0.0.1>nul
+echo type in key
+ping n- 2 127.0.0.1>nul
+echo Would you like to download now?!
+echo Type key or No
+set input=
+set /p input=
+if %input%== vanta3773 goto 1
+if %input%== No goto 2
+set input=
+
+set /p input=
+
+if %input%== vanta3773 goto 1
+:1
+Cls
+color A
+echo downloading thanks for your purchase
+ping n- 2 127.0.0.1>nul
+echo Downloading... 1%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Downloading... 13%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Downloading... 14%
+ping n- 2 127.0.0.1>nul
+echo Downloading... 16%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo ezfn.exe.
+ping n- 2 127.0.0.1>nul
+echo Download has been paused.
+pause
+echo slotted
+echo Type 1 to delete
+set input=
+set /p input=
+if %input%== 1 goto 4
+:2
+Cls
+echo do you want to still download
+echo Type 2 for Yes or type 3 for No
+set input=
+set /p input=
+if %input%== 2 goto 1
+if %input%== 3 goto 3
+:3
+Cls
+color C
+echo ok
+echo free 3 day key
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+goto 1
+:4
+Cls
+color C
+echo batch
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo  37%
+echo  43%
+ping n- 2 127.0.0.1>nul
+echo ezfn.exe
+ping n- 2 127.0.0.1>nul
+echo has been injected
+echo =!WARNING!=
+echo getting account
+pause
+echo added to desktop
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+echo Cleared.
+pause
+Cls
+color 4
+echo ezfn.exe
+ping n- 2 127.0.0.1>nul
+echo privet server added
+pause
+echo HWID.exe
+echo usage
+ping n- 2 127.0.0.1>nul
+echo !@#.4fa
+ping n- 2 127.0.0.1>nul
+Cls
+color A
+echo clearing
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo just so you know dont press anything
+ping n- 2 127.0.0.1>nul
+echo adding
+ping n- 2 127.0.0.1>nul
+echo In case you didn't know,
+ping n- 2 127.0.0.1>nul
+echo fortnite xbox 301270170521
+ping n- 2 127.0.0.1>nul
+echo getting key
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+color C
+echo good
+ping n- 2 127.0.0.1>nul
+echo Hack.exe Successful.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+color A
+echo adding files into system 32
+ping n- 2 127.0.0.1>nul
+echo getting info
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo close
+ping n- 2 127.0.0.1>nul
+Cls
+color F
+echo disabling anti virus
+ping n- 2 127.0.0.1>nul
+echo free key usage 3 days
+ping n- 2 127.0.0.1>nul
+echo running
+ping n- 2 127.0.0.1>nul
+echo fn
+ping n- 2 127.0.0.1>nul
+echo vanta loader.exe
+ping n- 2 127.0.0.1>nul
+echo loading vanta
+echo complete
+ping n- 2 127.0.0.1>nul
+echo 0000000000
+ping n- 2 127.0.0.1>nul
+echo #vantasupport
+ping n- 2 127.0.0.1>nul
+echo done
+ping n- 2 127.0.0.1>nul
+echo I want to let you know that you can now use you computer
+ping n- 2 127.0.0.1>nul
+echo Nothing was deleted.
+echo key vanta3773
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo 1
+ping n- 2 127.0.0.1>nul
+echo 10
+ping n- 2 127.0.0.1>nul
+echo 2
+ping n- 2 127.0.0.1>nul
+echo 6
+ping n- 2 127.0.0.1>nul
+echo WIN+H
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Later!
+ping n- 2 127.0.0.1>nul
+echo Press any button to exit after you're done reading.
+ping n- 2 127.0.0.1>nul
+pause
+exit
